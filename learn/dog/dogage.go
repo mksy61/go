87391a62a8ba83c2564cd37cecf789156a81bbe2
murataksoy61 <44951062+murataksoy61@users.noreply.github.com>
2020-06-24 17:45:00 +0300

    dogAge

--- learn/dog/dogage.go
+++ learn/dog/dogage.go
@@ -1,4 +1,4 @@
-//Packge Dog calculates your dogs human years age
+//Package dog calculates your dogs human years age
 package dog
 
 //Years function takes dog years as parameter and returns human years
